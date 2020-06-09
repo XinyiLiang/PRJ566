1e1fa18cdf49a53730e3b1320ead95d959be0afe
BogzzzonPC <you@example.com>
2020-06-09 17:43:54 -0400

    EventLogin(fixed)

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class  App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={require('./Images/detectiveLogo.png')} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Detective Game!</h1>
+                    <h1 className="App-title">Detective Game</h1>
                 </header>
 
             </div>
@@ -29,10 +29,11 @@ class  App extends Component {
                 
                        <Switch>
                             <Route exact path="/" component={Home} />
-                            <Route path="/about" component={About} />
+                         
                             <Route path="/Events" component={Events} />
                             <Route path="/Media" component={Media} />
                             <Route path="/Teams" component={Teams} />
+                            <Route path="/about" component={About} />
                             <Route path="/Account" component={Account} />
                             <Route component={NoMatch} />
                         </Switch>
