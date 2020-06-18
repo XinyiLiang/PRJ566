5eec50edb1ba9b904c3370960ae3b3ec765ea053
workoncodezhy <55505832+workoncodezhy@users.noreply.github.com>
2020-06-17 21:57:53 -0400

    testRegisterWithAccountPage

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Account = () =>(
         <div>
-            <h1> Account Page</h1>
+             <RegisterForm/>
             </div>
     )
 export default Account;
