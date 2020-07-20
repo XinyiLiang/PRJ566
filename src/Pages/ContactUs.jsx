9ae7ff2a6679aa397fb96238dd16acc5a7f068f8
XinyiLiang <Daisylxy12@gmail.com>
2020-07-20 17:15:00 -0400

    FitDiffScreenSize

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,91 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
-
-
-const Styles = styled.div`
-  
-/*Contact sectiom*/
-.content-header{
-  font-family: 'Oleo Script', cursive;
-  color:#fcc500;
-  font-size: 45px;
-}
-
-.section-content{
-  
-  
-  padding-top:10em;
-  text-align: left; 
-  
-
-}
-#contact{
-    
-  font-family: 'Teko', sans-serif;
-  padding-top: 30px;
-  
-  height: 38em;
-  
-  background: #3a6186; /* fallback for old browsers */
-  background: -webkit-linear-gradient(to left, #3a6186 , #55b5b3); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to left, #3a6186 , #55b5b3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    color : #fff;    
-}
-.contact-section{
-  padding-top: 40px;
-
-}
-.contact-section .col-md-6{
-    width: 100%;
-  }
-  
-  .form-line{
-    border-right: 1px solid #B29999;
-    padding-right:3em;
-  }
-  
-  .form-group{
-    margin-top: 10px;
-  }
-  label{
-    font-size: 1.3em;
-    line-height: 1em;
-    font-weight: normal;
-  }
-  .form-control{
-    font-size: 1.3em;
-    color: #080808;
-  }
-  textarea.form-control {
-      height: 135px;
-     /* margin-top: px;*/
-  }
-  .submit{
-    font-size: 1.1em;
-    float: right;
-    width: 150px;
-    background-color: transparent;
-    color: #fff;
-  
-  }
-  
-`;
+import {Container,Row,Col} from 'react-bootstrap';
 export const ContactUs = () =>(
 
-    <Styles>
+   
 
 <div id="contact">
    
-    <div class="container"> 
-    <div class="row">
+    <Container> 
+    <Row>
 			
-      
-      <div class="contact-section col-6">
-			{/*  */}
+    <Col md={6} xs={12} lg={6} className="section-content">
+				<h1 className="section-header">Get in <span className="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s"> Touch with us</span></h1>
+				<h3>Let us know how you feel on our game today!</h3>
+			</Col>
+
+    <Col md={6} xs={12} lg={6} className="contact-section ">
+			
               
 				<form >
                 
-					<div class=" form-line">
+					<div >
 			  			<div class="form-group">
 			  				<label for="exampleInputUsername">Your name</label>
 					    	<input type="text" class="form-control" id="" placeholder=" Enter Name"/>
@@ -108,22 +42,19 @@ export const ContactUs = () =>(
 			  				<button type="button" class="btn btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i>  Send Message</button>
 			  			</div>
 			  	
-                          </div>	
+            </div>	
                    
 				</form>
-			</div>
+			</Col>
 
-      <div class="section-content col-6">
-				<h1 class="section-header">Get in <span class="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s"> Touch with us</span></h1>
-				<h3>Let us know how you feel on our game today!</h3>
-			</div>
+      
 
 
-            </div>
-            </div>
+            </Row>
+            </Container>
 		</div>
    
- </Styles>
+
 
     )
 
