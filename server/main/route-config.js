63f7c305a19455803911ecbedc2a685eae0bb112
XinyiLiang <Daisylxy12@gmail.com>
2020-08-03 10:06:45 -0400

    newPullForLogin

--- server/main/route-config.js
+++ server/main/route-config.js
@@ -3,20 +3,6 @@ var router = express.Router();
 
 
 var clues  = require('../routes/clue');
-<<<<<<< HEAD
-var npc  = require('../routes/npcs');
- var gameday = require('../routes/gameday');
- var npm_Scenario = require('../routes/npc_scenario');
-var questions = require('../routes/question');
- var score = require('../routes/score');
- var team_answer = require('../routes/team_answer');
-var team_score = require('../routes/team_score');
- var team_Trophy = require('../routes/team_Trophy');
- var team = require('../routes/team');
-var user = require('../routes/user');
- var scenario = require('../routes/scenario');
- var trophys = require('../routes/trophy');
-=======
 var npc  = require('../routes/npc');
  var gameday = require('../routes/gameday');
  var npm_Scenario = require('../routes/npc_scenario');
@@ -29,7 +15,6 @@ var npc  = require('../routes/npc');
 var user = require('../routes/userRoutes');
  var scenario = require('../routes/scenario');
 var trophys = require('../routes/trophy');
->>>>>>> 9dc0f523b0d9e0fe296311dba3472c9b23d5e4fe
 
 
 
