4689e72062146ac2a21aed26ebc615e6806df9f6
BogzzzonPC <you@example.com>
2020-08-06 17:34:00 -0400

    TeamPage

--- client/src/components/TeamInfo.jsx
+++ client/src/components/TeamInfo.jsx
@@ -1,95 +1,167 @@
-import React from 'react';
-import 'antd/dist/antd.css';
+import React, { useEffect, useState,Prompt } from "react";
+import {BsFillLockFill} from "react-icons/bs";
+import { Form, Field } from 'react-advanced-form'
+import { Input, Button } from 'react-advanced-form-addons'
+import { Modal } from "react-bootstrap";
 
-import { Table, Typography } from 'antd';
 
-const { Text } = Typography;
+function TeamInfo() {
 
-const columns = [
-  {
-    title: 'Event Name',
-    dataIndex: 'eventName',
-  },
-  {
-    title: 'Date',
-    dataIndex: 'date',
-  },
-  {
-    title: 'Score',
-    dataIndex: 'score',
-  },
-  {
-    title: 'Ranking',
-    dataIndex: 'ranking',
-  },
-];
 
-const data = [
-  {
-    key: '1',
-    eventName: 'Indenpendence Day',
-    date: 'June 15',
-    score: 7500,
-    ranking: 3,
-  },
-  {
-    key: '2',
-    eventName: 'Tycoon Code',
-    date: 'June 22',
-    score:  10000,
-    ranking: 3,
-  },
-  {
-    key: '3',
-    eventName: 'Winner Cruise',
-    date: 'June 30',
-    score:  17500,
-    ranking: 1,
-  },
-  {
-    key: '4',
-    eventName: 'New Life',
-    date: 'July 6',
-    score:  12500,
-    ranking: 2,
-  },
-];
+  
+  const [Teams, dataSet] = useState([]);
+  const [password, setPassword] = useState("");
+  const[infoModalShow,infoSetModalShow] = React.useState(false);
 
-class TeamInfo extends React.Component {
-  render() {
-    return(
-<Table
-      columns={columns}
-      dataSource={data}
-      pagination={false}
-      bordered
-      summary={pageData => {
-        let totalScore = 0;
-        let totalRanking = 0;
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch('api/get/GetAllTeamInfo')
+      response = await response.json()
+      dataSet(response)
+      console.log(response);
+    }
+  
+    
+    fetchMyAPI();
+   
+  
+  }, [])
 
-        pageData.forEach(({ score, ranking }) => {
-          totalScore += score;
-        });
+  const editPlayerTeam = (team_id,email) => {
+    return fetch(`/api/put/userTeam/${email}`, {
+      method: 'PUT',
+      body: JSON.stringify({email:email,team_id:team_id}),
+      headers: {
+      'Content-Type': 'application/json'
+      }
+    })
+  };
 
-        return (
-          <>
-            <Table.Summary.Row>
-              <Table.Summary.Cell>Total</Table.Summary.Cell>
-              <Table.Summary.Cell></Table.Summary.Cell>
-              <Table.Summary.Cell>
-                <Text type="danger">{totalScore}</Text>
-              </Table.Summary.Cell>
-              <Table.Summary.Cell>
-                <Text>{totalRanking}</Text>
-              </Table.Summary.Cell>
-            </Table.Summary.Row>
-          </>
-        );
-      }}
-    />
-    );
+ function checkPlayer(data){
+
+  if(sessionStorage.getItem("auth") == "true"){
+      if(data.TYPE === 'public'){
+        sessionStorage.setItem("team",data.TEAM_ID);
+        console.log(sessionStorage.getItem("email"))
+        editPlayerTeam(data.TEAM_ID,sessionStorage.getItem("email"));
+        alert("Welcome to the Team (" + data.NAME + ")")
+      }else{
+        console.log(data);
+        return(
+
+          infoSetModalShow(true)
+        
+          )
+      }
   }
-}
 
- 
+
+ }
+  function passCheck(data,value) {
+      console.log(data,value)
+    if(data.PASSWORD == value){
+      editPlayerTeam(data.TEAM_ID,sessionStorage.getItem("email"));
+    }else{
+      return(alert("Wrong Password"));
+    }
+
+  }
+
+
+    return(
+      <>
+<div class="TeamList rounded">
+        
+    <div class="container rounded-top TeamListContainer">
+        <div class="row rounded-top TeamListHeader"> 
+                 <div class="col-6">
+                    <span class="font-weight-bold">Team Name</span>
+                 </div>
+                 <div class="col col-mu-auto">
+                    <span class="font-weight-bold">Member#</span>
+                 </div>
+                <div class="col col-md-auto">
+                    <span class="font-weight-bold"></span>
+                </div>
+                
+       </div>
+   
+       {Teams.map(data =>{
+        
+             if(data.TYPE === 'public'){
+              return(
+                 <div class="row TeamDetailsRow" key={`${data.TEAM_ID}`}> 
+                      <div class="col"> 
+                      <span class="col-6 TeamName"> {data.NAME} </span>
+                        </div>
+                        <div class="col mu-auto TeamCol"> 
+                         {data.TEAM_MEMBER_COUNT}
+                        </div>
+                        <div class="col md-auto text-right"> <button type="button" class="btn btn-outline-success btn-sm"
+                         onClick={()=>{ checkPlayer(data) }}
+                         >Join</button> </div>
+                 </div> 
+              )}
+              return(
+                <div class="row TeamDetailsRow" key={`${data.TEAM_ID}`}> 
+                      <div class="col"> 
+                      <span class="col-6 TeamName">  {data.NAME}&nbsp;<BsFillLockFill/> </span>
+                        </div>
+                        <div class="col mu-auto TeamCol"> 
+                        {data.TEAM_MEMBER_COUNT}
+                        </div>
+                        <div class="col md-auto text-right"> 
+                        <button type="button" class="btn btn-outline-success btn-sm" 
+                                onClick={()=>{checkPlayer(data) }}
+                        >
+                                  Join
+                        </button> 
+                        </div>
+
+                  <Modal show={infoModalShow} onHide={() => infoSetModalShow(false)} size ="sm" aria-labelledby="contained-modal-title-vcenter" centered>
+                  <Modal.Header closeButton>
+                      <Modal.Title id="contained-modal-title-vcenter">
+                          {data.NAME}
+                      </Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                  <Form >
+                    <Field.Group >
+                      <Input
+                        name="password"
+                        type="password"
+                        label="Password"
+                        onChange={e =>{setPassword(e.value);console.log(password)}}
+                      />
+                          <Button bsSize="lg" variant="success" type="submit" primary onClick={()=>passCheck(data,password)}  >Join</Button>  
+                    </Field.Group>      
+                    
+                    </Form>
+                  </Modal.Body>
+              </Modal>
+
+
+                 </div> 
+
+
+
+              )
+            }
+              )}
+           
+
+        
+        
+      
+       
+       </div>
+    </div>
+   
+    </>
+   );
+
+
+  }
+
+
 export default TeamInfo;
