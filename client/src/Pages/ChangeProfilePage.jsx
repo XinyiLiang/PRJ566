916d7c6a506f9dd2928a1e0c006c1dd0da57369d
XinyiLiang <Daisylxy12@gmail.com>
2020-08-09 22:56:02 -0400

    fix_ChangeUserProfile_Password_validation_problem

--- client/src/Pages/ChangeProfilePage.jsx
+++ client/src/Pages/ChangeProfilePage.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ChangePro from '../components/ChangeProfile';
 import {NavBar} from '../components/NavBar';
 
+import rules from '../ValidationRules'
+import messages from '../ValidationMessages'
+import { FormProvider } from 'react-advanced-form'
+
 export const ChangeProfilePage = () =>(
     <div className="HomeMain">
          <NavBar/>
-           
-    <ChangePro/>
-
+         <div className="ChangeProfileContainer">
+          <FormProvider rules={rules} messages={messages}>
+            <h1 >Update My Profile</h1>
+                    <ChangePro/>
+          </FormProvider>
+       </div>
     </div>
 
     )
